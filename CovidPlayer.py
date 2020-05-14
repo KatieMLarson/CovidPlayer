b74056349afc0223eb5f3038db65b7123ee1111b
KatieMLarson <katie.larson@thoughtworks.com>
2020-05-14 10:36:27 -0500

    [Katie/Jacob]initial commit to play random VLC clips

---
+++ CovidPlayer.py
@@ -0,0 +1,17 @@
+import subprocess
+import random
+from os import listdir
+from os.path import join, isfile
+
+mypath = '../Music/'
+
+def getSongs():
+    return [join(mypath,file) for file in listdir(mypath)
+        if file.endswith(('.mp3', '.mp4', '.wav'))]
+            
+songArray = getSongs()
+
+def chooseSong(songs):
+    return random.choice(songs)
+    
+subprocess.call(['cvlc', chooseSong(songArray)])
