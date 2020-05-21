8619b327641db3493db96b757e8bb6ef3f96bfb8
KatieMLarson <katie.larson@thoughtworks.com>
2020-05-21 10:45:17 -0500

    [Jacob/Katie] fix audio, clean up files, and finalize

--- Button.py
+++ Button.py
@@ -2,46 +2,24 @@ import pygame
 import CovidPlayer
 
 def main():
-    # initializes Pygame
     pygame.init()
-
-    # sets the window title
-    pygame.display.set_caption('Keyboard events')
-
-    # sets the window size
+    pygame.mixer.quit()
+    # Window needed to focus button click events
     pygame.display.set_mode((400, 400))
 
-    # infinite loop
     while True:
-        # gets a single event from the event queue
         event = pygame.event.wait()
-
-        # if the 'close' button of the window is pressed
         if event.type == pygame.QUIT:
-            # stops the application
             break
-
-        # captures the 'KEYDOWN' and 'KEYUP' events
         if event.type in (pygame.KEYDOWN, pygame.KEYUP):
-            # gets the key name
             key_name = pygame.key.name(event.key)
-
-            # converts to uppercase the key name
             key_name = key_name.upper()
-
-            # if any key is pressed
             if event.type == pygame.KEYDOWN:
-                # prints on the console the key pressed
                 print ('{} key pressed'.format(key_name))
-
-            # if any key is released
             elif event.type == pygame.KEYUP:
-                # prints on the console the released key
-                # run CovidPlayer
                 CovidPlayer.main()
                 print ('{} key released'.format(key_name))
 
-    # finalizes Pygame
     pygame.quit()
 
 
